0d0acc2b44e134a4e438e8ca844d67a0131098e0
Jason Shin <jason002009@gmail.com>
2023-05-17 09:21:04 -0700

    first commit

--- apps/game/pages/index.tsx
+++ apps/game/pages/index.tsx
@@ -1,5 +1,4 @@
 import Quizlet from 'dataset';
-import Fun from 'dataset/sets/Fun';
 import {
   CardSide,
   MediaType,
@@ -7,62 +6,138 @@ import {
   SerializedMediaImage,
   SerializedMediaText,
   StudiableItem,
+  StudiableCardSideLabel,
 } from 'dataset/types';
-import Image from 'next/image';
 
-const IMAGE_HEIGHT = 100;
-const IMAGE_WIDTH = 120;
+import s from './index.module.css';
+import React, { useState, useEffect } from 'react';
+
 export default function Game() {
-  // If you don't want SSR
-  if (typeof window !== 'undefined') return null;
-
   // to get a specific Set
-  const { disneyPrincessTrivia: quizletSet } = Fun.getAllSetsMap();
-  // const quizletSet = Quizlet.getRandomSet();
+  // const { disneyPrincessTrivia: quizletSet } = Fun.getAllSetsMap();
+  const quizletSet = Quizlet.getRandomSet();
 
-  const renderMedia = (media: SerializedMedia) => {
-    switch (media.type) {
-      case MediaType.TEXT:
-        const { plainText } = media as SerializedMediaText;
-        return <div key={media.type}>{plainText}</div>;
-      case MediaType.IMAGE:
-        const { url } = media as SerializedMediaImage;
-        return (
-          <Image
-            alt="term image"
-            height={IMAGE_HEIGHT}
-            key={media.type}
-            src={url}
-            width={IMAGE_WIDTH}
-          />
-        );
-    }
+  let itemIndex = 0;
+
+  const getNextWord = () => {
+    const labelCardSide = quizletSet.studiableItem[
+      itemIndex++
+    ].cardSides.filter(
+      cardside => cardside.label == StudiableCardSideLabel.WORD
+    )[0];
+
+    const text = labelCardSide.media[0] as SerializedMediaText;
+    const { plainText } = text;
+    return plainText;
   };
-  const renderTerm = (studiableItem: StudiableItem) => (
-    <div key={studiableItem.id}>
-      {studiableItem.cardSides.map(cardSide => {
-        const { label, media } = cardSide;
-        return (
-          <div key={cardSide.sideId}>
-            {label}: {media.map(termMedia => renderMedia(termMedia))}
-          </div>
-        );
-      })}
-    </div>
-  );
 
-  const renderTerms = (studiableItems: StudiableItem[]) => (
-    <>
-      <h3>({studiableItems.length} Terms)</h3>
-      {studiableItems.map(studiableItem => renderTerm(studiableItem))}
-    </>
-  );
+  const [timeLeft, setTimeLeft] = useState(10);
+  const [word, setWord] = useState('');
+
+  useEffect(() => {
+    // SETUP CANVAS
+    const canvas = document.getElementById(s.canvas) as HTMLCanvasElement;
+    const ctx = canvas.getContext('2d');
+
+    const canvasOffsetX = canvas.offsetLeft;
+    const canvasOffsetY = canvas.offsetTop;
+    canvas.width = window.innerWidth - canvasOffsetX;
+    canvas.height = window.innerHeight - canvasOffsetY;
+
+    let isPainting = false;
+    let lineWidth = 5;
+
+    // Draw on canvas
+    const onMouseMove = e => {
+      if (!isPainting || collapsed) {
+        return;
+      }
+
+      ctx.lineWidth = lineWidth;
+      ctx.lineCap = 'round';
+
+      ctx.lineTo(e.clientX - canvasOffsetX - 30, e.clientY - 30);
+      ctx.stroke();
+    };
+
+    canvas.addEventListener('mousedown', e => {
+      isPainting = true;
+    });
+
+    canvas.addEventListener('mouseup', e => {
+      isPainting = false;
+      ctx.stroke();
+      ctx.beginPath();
+    });
+
+    canvas.addEventListener('mousemove', onMouseMove);
+
+    // Setup Toolbar
+    const clearButton = document.getElementById('clear');
+    const nextButton = document.getElementById('next');
+    const quizModeButton = document.getElementById('quizmode');
+    const word = document.getElementById('word');
+
+    const onClearButtonClick = () => {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+    };
+
+    const onNextButtonClick = () => {
+      collapsed = false;
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      setWord(getNextWord());
+      timeLeftValue = startingTime;
+      setTimeLeft(timeLeftValue);
+    };
+
+    clearButton.addEventListener('click', onClearButtonClick);
+    nextButton.addEventListener('click', onNextButtonClick);
+
+    // Setup Countdown
+    const countdown = document.getElementById('countdown');
+
+    const startingTime = 10;
+    let timeLeftValue = 10;
+    let collapsed = false;
+
+    const updateTime = () => {
+      if (timeLeftValue == 0) {
+        collapsed = true;
+        return;
+      }
+      timeLeftValue--;
+      setTimeLeft(timeLeftValue);
+    };
+
+    let isTimerRunning = false;
+
+    const onQuizModeButtonClick = () => {
+      if (isTimerRunning) return;
+      setInterval(updateTime, 1000);
+      isTimerRunning = true;
+      setWord(getNextWord());
+    };
+
+    quizModeButton.addEventListener('click', onQuizModeButtonClick);
+  }, []);
 
   return (
-    <div>
-      <h1>Your game title here!</h1>
-      <h2>Set used: {quizletSet.set.title}</h2>
-      {renderTerms(quizletSet.studiableItem)}
+    <div className={s.container}>
+      <div id={s.toolbar}>
+        <h1>
+          Word: <span id="word">{word}</span>
+        </h1>
+        <h2>
+          Time: <span id="countdown">{timeLeft}</span>
+        </h2>
+        <button id="clear">Clear</button>
+        <button id="next">Next</button>
+        <br />
+        <button id="quizmode">Quiz Mode</button>
+      </div>
+      <div className="drawing-board">
+        <canvas id={s.canvas}></canvas>
+      </div>
     </div>
   );
 }
